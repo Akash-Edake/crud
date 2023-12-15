e018f66dc665010922ab8819c0b0eadad7c3b187
Akash-Edake <akashedake30@gmail.com>
2023-12-15 23:33:03 +0530

    records api

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require("./src/db/mongoose");
 const express = require("express");
+const cors = require("cors");
 const records = require("./src/routers/records");
 
 const port = process.env.PORT || 8080;
 const app = express();
+app.use(cors())
 app.use(express.json());
 app.use(records);
 
