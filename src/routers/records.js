2b76b609e778953489fd5cc5ba59b3c7c715f3d6
Akash-Edake <akashedake30@gmail.com>
2023-12-16 10:03:52 +0530

    adding Graceful shutdown, helmet-for security, compression-decrease the size

--- src/routers/records.js
+++ src/routers/records.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { findAll,add,deleteReconds,updateRecords } = require("../controllers/records");
 
-router.get("/recodrs", findAll);
+router.get("/records", findAll);
 router.post("/add", add);
 router.post("/delete", deleteReconds);
 router.put("/update", updateRecords);
